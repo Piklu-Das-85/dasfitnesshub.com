3b8655016a4caeeacb0ae3de6008ab328481de2d
pikludas_bitbucket <pikluranjandas@gmail.com>
2021-01-26 11:42:00 +0530

    Nineth Commit: Modified the alb-listner.tf

--- alb-listner.tf
+++ alb-listner.tf
@@ -8,13 +8,13 @@ resource "aws_lb_listener" "dfsc_https" {
   certificate_arn = "arn:aws:acm:us-east-1:015148330366:certificate/17eacedc-d24c-4ad1-af0d-7eca4a46c301"
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.dfsc-fron-end-tg.arn
+    target_group_arn = aws_lb_target_group.dfsc-front-end-tg.arn
   }
 }
 
 # Create ALB Listner Backend Rule - HTTPS
 
-resource "aws_lb_listener_rue" "dfsc_admin_https" {
+resource "aws_lb_listener_rule" "dfsc_admin_https" {
   listener_arn = aws_lb_listener.dfsc_https.arn 
   priority = 100
   action {
